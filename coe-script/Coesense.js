2b65fe89014b1a22e7a1c9f8ceecded357b6514f
Loic Huve <huve.loic@gmail.com>
2012-08-04 00:52:37 +0200

    feed sort

--- coe-script/Coesense.js
+++ coe-script/Coesense.js
@@ -5,6 +5,12 @@ function Coesense(){
 
 Coesense.prototype.init = function(){
 	this.$container.isotope({
-		itemSelector: '.coe-main_feed-item'
+		itemSelector: '.coe-main_feed-item',
+		getSortData : {
+			id : function ( $elem ) {
+				return parseInt( $elem.attr('data-id') );
+			}
+		},
+		sortBy: 'id'
 	});
 }
