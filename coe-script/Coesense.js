b337ceb67628eee3a04bd7bf3adc7bef59cfa8a9
Loic Huve <huve.loic@gmail.com>
2012-08-05 23:51:09 +0200

    single post display, easing

--- coe-script/Coesense.js
+++ coe-script/Coesense.js
@@ -4,6 +4,8 @@ function Coesense(){
 }
 
 Coesense.prototype.init = function(){
+	var _self = this;
+	//feed setup
 	this.$container.isotope({
 		itemSelector: '.coe-main_feed-item',
 		getSortData : {
@@ -12,5 +14,43 @@ Coesense.prototype.init = function(){
 			}
 		},
 		sortBy: 'id'
+	});
+
+	//hover behaviour on articles
+	$("article").hover(function(){
+		$(this).css({cursor: "pointer"});
+		$("article").not($(this)).animate({opacity: .5}, 50, 'easeOutExpo');
+	}, function(){
+		$("article").not($(this)).animate({opacity: 1}, 50, 'easeInExpo');
+	});
+
+	//click behaviour on an article
+	$("article").click(function(){
+		_self.loadSingle($(this));		
+	});
+
+	//click behaviour on closing the single panel
+	$("#coe-main_single-close").click(function(){
+		$("#coe-main_single").removeClass("open").animate({left: -350}, 300, 'easeInCirc');
+		$("#coe-main_feed").animate({left: 0}, 300, 'easeInCirc');
+	});
+}
+
+Coesense.prototype.loadSingle = function($this){
+	var $self = $this;
+	//$("#coe-ui_overlay").fadeIn("fast");
+	$("#coe-main_single-wrapper").fadeOut("fast", function(){ 
+		$("#coe-main_single-wrapper").empty() 
+		$.when(
+			$("#coe-main_single-wrapper")
+				.append($self.find("h1").clone())
+				.append($self.find(".coe-main_item-infos_source").clone())
+				.append($self.find(".coe-main_item-infos_tags").clone())
+		).done(function(){
+			//$("#coe-ui_overlay").fadeOut("fast");
+			$("#coe-main_single").addClass("open").animate({left: 0}, 300, 'easeOutCirc');
+			$("#coe-main_feed").animate({left: 350}, 300, 'easeOutCirc');
+			$("#coe-main_single-wrapper").fadeIn("fast");
+		});
 	});
 }
